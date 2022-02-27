d8445186ae9f75e749f858e3b5f0a57cfb52ad04
Israel Gadelha <gdelha.israel@gmail.com>
2022-02-27 12:51:40 -0300

    Atualização do código com switch

--- codigo_semaforo_switch.ino/codigo_semaforo_switch.ino.ino
+++ codigo_semaforo_switch.ino/codigo_semaforo_switch.ino.ino
@@ -6,6 +6,7 @@
 #define ledVerdeTB 11
 
 int estado = 0;
+int inicial = 0;
 boolean activeTA = false;
 boolean activeTB = false;
 
@@ -16,9 +17,13 @@ void setup() {
   pinMode(ledVermelhoTB, OUTPUT);
   pinMode(ledAmareloTB, OUTPUT);
   pinMode(ledVerdeTB, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  if(digitalRead(ledVerdeTA) == 0 && digitalRead(ledVerdeTB) == 0 && digitalRead(ledAmareloTA) == 0 && digitalRead(ledAmareloTB) == 0){
+    inicial = 1;
+  }
 
   if(digitalRead(2) == 1 && digitalRead(3) == 0){
     estado = 1;
@@ -30,43 +35,60 @@ void loop() {
 
   switch(estado){
     case 1:
-      digitalWrite(ledVerdeTA, HIGH);
-      digitalWrite(ledVermelhoTB, HIGH);
-      activeTA = true;
-      activeTB = false;
-      delay(10000);
-      break;
-    case 2:
-      digitalWrite(ledVermelhoTA, HIGH);
-      digitalWrite(ledVerdeTB, HIGH);
-      activeTB = true;
-      activeTA = false;
-      delay(10000);
-      break;
-    case 3:
-        //Semafóros
-        digitalWrite(ledVerdeTA, HIGH);
+      if(digitalRead(ledVerdeTA) == 1){
         digitalWrite(ledVermelhoTB, HIGH);
-        delay(10000);
-   
-        digitalWrite(ledVerdeTA, LOW);
-        digitalWrite(ledAmareloTA, HIGH);
-        delay(5000);
-        
-        digitalWrite(ledAmareloTA, LOW);
-        digitalWrite(ledVermelhoTB, LOW);
-      
-        digitalWrite(ledVermelhoTA, HIGH);
-        digitalWrite(ledVerdeTB, HIGH);
-        delay(10000);
-        
+        digitalWrite(ledVerdeTA, HIGH);
+        activeTB = false;
+        activeTA = true;
+        delay(3000);
+      } else {
         digitalWrite(ledVerdeTB, LOW);
         digitalWrite(ledAmareloTB, HIGH);
-        delay(5000);
-      
-        digitalWrite(ledVermelhoTA, LOW);
+        delay(1000);
         digitalWrite(ledAmareloTB, LOW);
+        digitalWrite(ledVermelhoTB, HIGH);
+        digitalWrite(ledVerdeTA, HIGH);
+        digitalWrite(ledVermelhoTA, LOW);
+        delay(3000);
       }
       break;
+    case 2:
+      if(digitalRead(ledVerdeTB) == 1){
+        digitalWrite(ledVermelhoTA, HIGH);
+        digitalWrite(ledVerdeTB, HIGH);
+        delay(3000);
+      } else {
+        digitalWrite(ledVerdeTA, LOW);
+        digitalWrite(ledAmareloTA, HIGH);
+        delay(1000);
+        digitalWrite(ledAmareloTA, LOW);
+        digitalWrite(ledVermelhoTA, HIGH);
+        digitalWrite(ledVerdeTB, HIGH);
+        digitalWrite(ledVermelhoTB, LOW);
+        delay(3000);
+      }
+      break;
+    case 3:
+      if(digitalRead(ledVerdeTA) == 1 || inicial == 1){
+        digitalWrite(ledVerdeTA, LOW);
+        digitalWrite(ledAmareloTA, HIGH);
+        delay(1000);
+        digitalWrite(ledAmareloTA, LOW);
+        digitalWrite(ledVermelhoTA, HIGH);
+        digitalWrite(ledVerdeTB, HIGH);
+        digitalWrite(ledVermelhoTB, LOW);
+        delay(3000);
+        inicial = 0;
+      }
+      if(digitalRead(ledVerdeTB) == 1){
+        digitalWrite(ledVerdeTB, LOW);
+        digitalWrite(ledAmareloTB, HIGH);
+        delay(1000);
+        digitalWrite(ledAmareloTB, LOW);
+        digitalWrite(ledVermelhoTB, HIGH);
+        digitalWrite(ledVerdeTA, HIGH);
+        digitalWrite(ledVermelhoTA, LOW);
+        delay(3000);
+      }
   }
 }
